b39e36ae3592b99d13f9757d783ae1f502066962
SoeZeng <zengyishu213@163.com>
2022-05-14 23:07:03 +0800

    SeventhCommit_add follow function

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -4,11 +4,36 @@ $(function(){
 
 function follow() {
 	var btn = this;
+	// 通过样式切换来判断是关注or取消关注
 	if($(btn).hasClass("btn-info")) {
 		// 关注TA
-		$(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
+		$.post(
+			CONTEXT_PATH + "/follow",
+			{"entityType":3,"entityId":$(btn).prev().val()}, //当前按钮前一个标签的值
+			function (data) {
+				data = $.parseJSON(data);
+				if(data.code == 0) {
+					window.location.reload();
+				} else {
+					alert(data.msg);
+				}
+			}
+		);
+		// $(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
 	} else {
 		// 取消关注
-		$(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
+		$.post(
+			CONTEXT_PATH + "/unfollow",
+			{"entityType":3,"entityId":$(btn).prev().val()}, //当前按钮前一个标签的值
+			function (data) {
+				data = $.parseJSON(data);
+				if(data.code == 0) {
+					window.location.reload();
+				} else {
+					alert(data.msg);
+				}
+			}
+		);
+		// $(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
 	}
 }
